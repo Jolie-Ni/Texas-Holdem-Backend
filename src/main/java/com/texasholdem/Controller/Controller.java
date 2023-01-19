3d3c9308dfe069d318c602bdd49e31a35d47ded6
Jolie Ni <ninan980805@gmail.com>
2023-01-18 22:36:52 -0500

    connected to mongoDB

---
+++ src/main/java/com/texasholdem/Controller/Controller.java
@@ -0,0 +1,42 @@
+package com.texasholdem.Controller;
+
+import com.texasholdem.Entity.Strategy;
+import com.texasholdem.Repository.StrategyRepository;
+import com.texasholdem.Service.StrategyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/strategy")
+public class Controller {
+
+    @Autowired
+    private StrategyService strategyService;
+    @PutMapping("/save")
+    public void save(@RequestBody Strategy strategy) {
+        strategyService.saveStrategy(strategy);
+    }
+
+    @GetMapping("/getall")
+    public List<Strategy> getAll(){
+        return strategyService.getAll();
+    }
+
+    @GetMapping("/get")
+    public Strategy get(@RequestParam("gamestate") String gameState){
+        if(strategyService.getStrategyWithGameState(gameState).isPresent()){
+            return strategyService.getStrategyWithGameState(gameState).get();
+        }else{
+            return null;
+        }
+    }
+
+    @GetMapping("/health")
+    public String health(){
+        return "UP";
+    }
+
+
+}
